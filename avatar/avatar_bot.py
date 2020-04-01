329ff1d2358bd215e6fb91276ad86c567b8dbcd1
Megumi_fox <i@megumifox.com>
2020-04-01 14:16:49 +0800

    add white background

--- avatar/avatar_bot.py
+++ avatar/avatar_bot.py
@@ -26,26 +26,36 @@ last = datetime.now(timezone.utc) - timedelta(hours = 2)
 
 def conv_sticker(file):
     im = Image.open(file).convert("RGBA")
-    im.save("/tmp/sticker.png","png")
+    im.save("/tmp/avatar.png","png")
     os.remove(file)
 
 def conv_gif(file):
     im = Image.open(file)
     iter = ImageSequence.Iterator(im)
-    iter[2].save('/tmp/noirgif.png')
+    iter[2].save('/tmp/avatar.png')
     os.remove(file)
 
 def conv_mp4(file):
     try:
         videoCapture = cv2.VideoCapture(file)
         success, frame = videoCapture.read()
-        cv2.imwrite('/tmp/video.png',frame)
+        cv2.imwrite('/tmp/avatar.png',frame)
     finally:
         videoCapture.release()
         os.remove(file)
 
-@client.on(events.NewMessage(chats=channel_entity,pattern=r'/avatar'))
-async def handler(event):
+def img2png(file):
+    im = Image.open(file).convert("RGBA")
+    im.save("/tmp/avatar.png","png")
+    os.remove(file)
+
+def conv_white(file):
+    im = Image.open(file).convert("RGBA")
+    wbg = Image.new("RGBA", im.size,color=(255,255,255,255))
+    wbg = Image.alpha_composite(wbg, im)
+    wbg.save(file)
+
+async def avatar(event,white=False):
     global last
     if (event.message.date - last) < timedelta(seconds = 100):
         t = (timedelta(seconds = 100) - (event.message.date -last)).total_seconds()
@@ -61,29 +71,26 @@ async def handler(event):
                 file_name = '/tmp/sticker.webp'
                 await client.download_media(message=replymsg,file=file_name)
                 conv_sticker(file_name)
-                upload_file_result = await client.upload_file(file='/tmp/sticker.png')
-                os.remove('/tmp/sticker.png')
             elif replymsg.file.mime_type == "video/mp4":
                 file_name = '/tmp/video.mp4'
                 await client.download_media(message=replymsg,file=file_name)
                 conv_mp4(file_name)
-                upload_file_result = await client.upload_file(file='/tmp/video.png')
-                os.remove('/tmp/video.png')
             elif replymsg.file.mime_type == "image/gif":
                 file_name = '/tmp/noirgif.gif'
                 await client.download_media(message=replymsg,file=file_name)
                 conv_gif(file_name)
-                upload_file_result = await client.upload_file(file='/tmp/noirgif.png')
-                os.remove('/tmp/noirgif.png')
             elif "image" in replymsg.file.mime_type:
                 file_name = '/tmp/image'+ replymsg.file.ext
                 await client.download_media(message=replymsg,file=file_name)
-                upload_file_result = await client.upload_file(file=file_name)
-                os.remove(file_name)
+                img2png(file_name)
             else:
                 m = await event.reply("不支持的類型")
                 return None
 
+        if white:
+            conv_white('/tmp/avatar.png')
+        upload_file_result = await client.upload_file(file='/tmp/avatar.png')
+        os.remove('/tmp/avatar.png')
         input_chat_uploaded_photo = InputChatUploadedPhoto(upload_file_result)
         result = await client(EditPhotoRequest(channel=event.message.to_id,
         photo=input_chat_uploaded_photo))
@@ -92,9 +99,19 @@ async def handler(event):
         m = await event.reply("你的頭呢？")
     return None
 
-try:
-    print('(Press Ctrl+C to stop this)')
-    client.run_until_disconnected()
-finally:
-    client.disconnect()
+
+@client.on(events.NewMessage(chats=channel_entity,pattern=r'/avatar_black'))
+async def handler(event):
+     await avatar(event)
+
+@client.on(events.NewMessage(chats=channel_entity,pattern=r'/avatar_white'))
+async def handler(event):
+    await avatar(event,white=True)
+
+if __name__ == "__main__":
+    try:
+        print('(Press Ctrl+C to stop this)')
+        client.run_until_disconnected()
+    finally:
+        client.disconnect()
 
