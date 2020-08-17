290233450cf2f3674c37be1515321540da9e0488
Megumi_fox <i@megumifox.com>
2020-08-17 18:50:04 +0800

    pixivdownloader: add cmd /pixiv_id

--- pixivdownloader/pixivdownloader.py
+++ pixivdownloader/pixivdownloader.py
@@ -22,7 +22,8 @@ bot_token = ''
 bot_name=''
 _USERNAME = ""
 _PASSWORD = ""
-my_id = 
+whitelist_user = []
+chat_list = []
 directory = "dl"
 artworks_root = "https://www.pixiv.net/artworks/"
 pixiv = AppPixivAPI_wrap()
@@ -32,7 +33,7 @@ client = TelegramClient(bot_name,
                     api_hash,
                     )
 
-async def pixiv_download(image_id):
+async def pixiv_download(image_id, nsfw=True):
     logger.info("image_id:%s",image_id)
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -43,7 +44,12 @@ async def pixiv_download(image_id):
         urls = await pixiv.getImageUrl(illust)
     else:
         raise ValueError("Cannot fetch illust")
+
     names = []
+
+    if (not nsfw and ('R-18' in illust['tags'][0]['name'])):
+        raise ValueError("lsp")
+
     for url in urls:
             url_basename = os.path.basename(url)
             name = url_basename
@@ -61,8 +67,7 @@ async def handler(event):
 
 @client.on(events.NewMessage(func=lambda e: e.is_private))
 async def handler(event):
-    print(event.sender_id)
-    if event.sender_id != my_id:
+    if event.sender_id not in whitelist_user:
         m = await event.reply("=.=")
         return None
     else:
@@ -70,17 +75,42 @@ async def handler(event):
         logger.debug(image_ids)
         if image_ids == []:
             return 0
-        mr = await event.reply("Downloading 。。。")
+        mr = await event.reply("Downloading ...")
         for image_id in image_ids:
-            try:
-                picnames,title = await pixiv_download(int(image_id))
-                for picname in picnames:
-                    m = await event.reply("title: {}\nfilename: {}\npixiv_id: [{}]({})".format(title,picname,image_id,artworks_root+image_id),force_document=True,file=os.path.join(directory,picname))
-            except BaseException as e:
-                logger.error(e)
-                m = await event.reply("´_>`, {}".format(e))
+            if len(image_id) > 5:
+                try:
+                    picnames,title = await pixiv_download(int(image_id))
+                    for picname in picnames:
+                        m = await event.reply("title: {}\nfilename: {}\npixiv_id: [{}]({})".format(title,picname,image_id,artworks_root+image_id),force_document=True,file=os.path.join(directory,picname))
+                except BaseException as e:
+                    logger.error(e)
+                    m = await event.reply("´_>`, {}".format(e))
         await mr.delete()
 
+@client.on(events.NewMessage(func=lambda e: not e.is_private,pattern=r'/pixiv_id'))
+async def handler(event):
+        if event.chat_id not in chat_list:
+            m = await event.reply("如果需要使用請先聯系 @MarvelousBlack 將該羣加入白名單。chat_id={}".format(event.chat_id))
+            return None
+        image_ids = re.findall('(\d+)',event.message.message)
+        logger.debug(image_ids)
+        if image_ids == []:
+            m = await event.reply("找不到圖片id")
+            return 0
+        mr = await event.reply("Downloading ...")
+        for image_id in image_ids:
+            if len(image_id) > 5:
+                try:
+                    picnames,title = await pixiv_download(int(image_id), nsfw=False)
+                    for picname in picnames:
+                        m = await event.reply("title: {}\nfilename: {}\npixiv_id: [{}]({})".format(title,picname,image_id,artworks_root+image_id),force_document=False,file=os.path.join(directory,picname))
+                        if 'p1' in picname:
+                            break
+                except BaseException as e:
+                    logger.error(e)
+                    m = await event.reply("´_>`, {}".format(e))
+        await mr.delete()
+            
 
 async def main():
     await pixiv.login(_USERNAME, _PASSWORD)
